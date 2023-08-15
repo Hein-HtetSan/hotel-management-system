13d9e3c0e97e4fda0c725929ad078a2a0c3403af
Hein-HtetSan <heinhtetsan33455@gmail.com>
2023-08-15 09:25:34 +0630

    added booking controller java

---
+++ src/encapsulated/booking.java
@@ -0,0 +1,117 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package encapsulated;
+
+/**
+ *
+ * @author acer
+ */
+public class booking {
+
+    /**
+     * @return the bookID
+     */
+    public int getBookID() {
+        return bookID;
+    }
+
+    /**
+     * @param bookID the bookID to set
+     */
+    public void setBookID(int bookID) {
+        this.bookID = bookID;
+    }
+
+    /**
+     * @return the userID
+     */
+    public int getUserID() {
+        return userID;
+    }
+
+    /**
+     * @param userID the userID to set
+     */
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    /**
+     * @return the roomID
+     */
+    public String getRoomID() {
+        return roomID;
+    }
+
+    /**
+     * @param roomID the roomID to set
+     */
+    public void setRoomID(String roomID) {
+        this.roomID = roomID;
+    }
+
+    /**
+     * @return the start
+     */
+    public String getStart() {
+        return start;
+    }
+
+    /**
+     * @param start the start to set
+     */
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    /**
+     * @return the end
+     */
+    public String getEnd() {
+        return end;
+    }
+
+    /**
+     * @param end the end to set
+     */
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    /**
+     * @return the noOfBed
+     */
+    public int getNoOfBed() {
+        return noOfBed;
+    }
+
+    /**
+     * @param noOfBed the noOfBed to set
+     */
+    public void setNoOfBed(int noOfBed) {
+        this.noOfBed = noOfBed;
+    }
+
+    /**
+     * @return the notes
+     */
+    public String getNotes() {
+        return notes;
+    }
+
+    /**
+     * @param notes the notes to set
+     */
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+    private int bookID;
+    private int userID;
+    private String roomID;
+    private String start;
+    private String end;
+    private int noOfBed;
+    private String notes;
+}
